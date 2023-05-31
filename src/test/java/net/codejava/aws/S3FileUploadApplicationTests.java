08a3a5009b2d42d64b31bbbab13e3914fd534b21
Olena_Krasylshchykova <olena_krasylshchykova@epam.com>
2023-05-31 12:54:42 +0200

    unit test for download

--- src/test/java/net/codejava/aws/S3FileUploadApplicationTests.java
+++ src/test/java/net/codejava/aws/S3FileUploadApplicationTests.java
@@ -1,10 +1,28 @@
 package net.codejava.aws;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.s3.S3Client;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import io.findify.s3mock.S3Mock;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.BufferedOutputStream;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import software.amazon.awssdk.regions.Region;
 
 @SpringBootTest
 class S3FileUploadApplicationTests {
@@ -13,7 +31,67 @@ class S3FileUploadApplicationTests {
 	void contextLoads() {
 	}
 
+	private final S3Client s3Client = createS3Client();
+	private final S3Mock api = createApi();
+	String bucketName = "my-test-bucket";
+	String objectKey = "my-object.txt";
 
+	private static S3Mock createApi() {
+		return new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
+	}
+
+	private static S3Client createS3Client() {
+		return S3Client.builder()
+				.region(Region.US_EAST_1)
+				.endpointOverride(URI.create("http://localhost:8001"))
+				.build();
+	}
+	@BeforeEach
+	public void setup() {
+		api.start();
+		EndpointConfiguration endpoint = new EndpointConfiguration("http://localhost:8001", "us-west-2");
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+
+		CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+				.bucket(bucketName)
+				.build();
+
+		CreateBucketResponse createBucketResponse = s3Client.createBucket(createBucketRequest);
+
+		System.out.println("Bucket created: " + createBucketResponse.location());
+
+	}
+
+	@Test
+	public void testDownloadFile() throws IOException {
+		Path filePath = Paths.get("src/main/resources/my-object.txt");
+
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+				.bucket(bucketName)
+				.key(objectKey)
+				.build();
+
+		s3Client.putObject(putObjectRequest, filePath);
+
+		System.out.println("Object uploaded successfully");
+
+		S3Util.downloadFile("my-object.txt", s3Client,"my-test-bucket");
+		Path downloadedFilePath = Paths.get("my-object.txt");
+		assertTrue(Files.exists(downloadedFilePath), "Downloaded file exists");
+
+		byte[] originalFileBytes = Files.readAllBytes(filePath);
+		byte[] downloadedFileBytes = Files.readAllBytes(downloadedFilePath);
+		assertArrayEquals(originalFileBytes, downloadedFileBytes, "Downloaded file contents match the original file");
+
+		Files.delete(downloadedFilePath);
+		assertFalse(Files.exists(downloadedFilePath), "Downloaded file has been deleted");
+	}
+
+	@AfterEach
+	public void cleanup() {
+		api.stop();
+	}
 }
 
 
